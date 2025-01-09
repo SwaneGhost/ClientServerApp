afeb7c3031c54d8ed07c010b7d6f46b7d7c8f718
Denis Streltsovski <den.stre9@gmail.com>
2025-01-09 22:29:34 +0200

    Pushed progress while working together
    all key points and thoughts saved on todo tags to revise on another day

--- Code/Client.py
+++ Code/Client.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
     pass
+
+# TODO check the reading size of the message
--- Code/Server.py
+++ Code/Server.py
@@ -2,6 +2,8 @@ import socket
 import struct
 import time
 import threading
+import select
+
 
 class Server:
     MAGIC_COOKIE = 0xabcddcba
@@ -10,65 +12,99 @@ class Server:
     PAYLOAD = 0x4
 
     def __init__(self):
-        # TODO - change hardcoded values
-        self.address = ''
+        self.address = socket.gethostbyname(socket.gethostname())
         self.udp_port = 0
         self.tcp_port = 0
-        self.running = False
+        self.running = threading.Event()
         self.UDP_socket = None
+        self.TCP_socket = None
 
     def start(self):
-        """
-        Start the server.
-
-        :return: None
-        """
-        self.running = True
+        self.running.set()
         print(f'Server started, listening on IP address {self.address}')
 
-        # Create a UDP socket
         try:
             self.UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.UDP_socket.bind((self.address, 0))
             self.udp_port = self.UDP_socket.getsockname()[1]
+
+            self.TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.TCP_socket.bind((self.address, 0))
+            self.tcp_port = self.TCP_socket.getsockname()[1]
         except socket.error as e:
             print(f'Error: {e}')
-        finally:
-            self.running = False
+            self.stop()
             return
 
-        # Open broadcast_offers as a thread
+        print(f'UDP port: {self.udp_port}, TCP port: {self.tcp_port}')
+
         broadcast_thread = threading.Thread(target=self.broadcast_offers)
         broadcast_thread.start()
 
+        listen_TCP_thread = threading.Thread(target=self.listen_for_TCP_connections)
+        listen_TCP_thread.start()
+
+        listen_UDP_thread = threading.Thread(target=self.listen_for_UDP_connections)
+        listen_UDP_thread.start()
+
+        # TODO multithreading doesn't work
+        try:
+            self.running.wait()
+        except KeyboardInterrupt:
+            print("Keyboard interrupt received, stopping server.")
+            self.stop()
 
     def broadcast_offers(self):
         """
-        Broadcasts offers to all clients using UDP.
-        The server broadcasts messages every second.
+        Broadcasts the server's IP address, UDP port and TCP port to the network
+
         :return: None
         """
-
-        # Pack the data into a binary format
+        # Create the message to broadcast
         message = struct.pack('!I B H H', Server.MAGIC_COOKIE, Server.OFFER, self.udp_port, self.tcp_port)
-
-        # While the server is running
-        while self.running:
-            # Create a UDP socket for broadcasting
+        # TODO fix this while condition
+        while self.running.is_set():
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as socket_for_broadcast:
-                # 50,000 is an arbitrary port number
-                # Client will sniff out from all ports the broadcast
-                socket_for_broadcast.sendto(message, ('255.255.255.255', 50000))
-            # Sleep for 1 second
+                socket_for_broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                try:
+                    socket_for_broadcast.sendto(message, ('<broadcast>', 50000))
+                except socket.error as e:
+                    print(f"Broadcast error: {e}")
             time.sleep(1)
 
-    def listen_for_TCP_requests(self):
+    def stop(self):
+        self.running.clear()
+        if self.UDP_socket:
+            try:
+                self.UDP_socket.close()
+            except socket.error as e:
+                print(f"Error closing UDP socket: {e}")
+
+        if self.TCP_socket:
+            try:
+                self.TCP_socket.close()
+            except socket.error as e:
+                print(f"Error closing TCP socket: {e}")
+        print('Server stopped')
+
+    def listen_for_TCP_connections(self):
         pass
 
-    def listen_for_UDP_requests(self):
+    def listen_for_UDP_connections(self):
+        # TODO fix this while condition
+        while self.running.is_set():
+            readable, _, _ = select.select([self.UDP_socket], [], [], 1)
+            for sock in readable:
+                data, addr = sock.recvfrom(1024)
+                print(f"Received UDP message from {addr}")
+                self.handle_UDP_request(sock)
+
+    def handle_TCP_request(self, sock: socket.socket):
         pass
 
-
+    def handle_UDP_request(self, sock: socket.socket):
+        pass
 
 if __name__ == '__main__':
-    pass
+    server = Server()
+    server.start()
